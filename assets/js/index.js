11c327ed729241e9be139b737370bb78ef005804
Olena Zdanovska <freshjs.ftm@gmail.com>
2023-01-02 11:04:31 +0200

    Add querySelectorAll & getElementsByClassName

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,26 +1,15 @@
 "use strict";
 
 function showMessage(msg = "1111") {
-  console.log("hi!");
   console.log(msg);
 }
 
-const action = () => {
-  showMessage("sdfsdfsdsfd");
-};
+//tag
+const lisTag = document.getElementsByTagName('li')
 
-//отримати кнопку
-//const button = document.getElementsByTagName('button')[0];
-//const [button] = document.getElementsByTagName("button");
-const button = document.querySelector('button+button');
-//натиснути на кнопку  і викликати функцію
-button.addEventListener('click', ()=>showMessage('qwerty'));
-// button.addEventListener("click", () => {
-//   showMessage("sdfsdfsdsfd");
-// });
+//class
+const lisClass = document.getElementsByClassName('item')
 
-
-const heading1 = document.getElementById('heading');
-
-//const heading2 = document.querySelector('#heading');
-const a = document.querySelector('a');
+//all li
+const lisAll = document.querySelectorAll('.item');
+const ps = document.querySelectorAll('article>p');
