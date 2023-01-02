75b0d906f238342cf34a299ba202f4d6b5ce86ed
Olena Zdanovska <freshjs.ftm@gmail.com>
2023-01-02 11:33:25 +0200

    Resolve task

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,6 +1,14 @@
 "use strict";
 
-function showMessage(msg = "1111") {
-  console.log(msg);
-}
+// v1  a
+const [active1] = document.getElementsByClassName('active');
+// v2  a
+const active2 = document.querySelector('.active');
 
+
+//v1 ps
+const ps = document.querySelectorAll('h2+p');
+
+//const ps = document.getElementsByTagName('h2+p');
+
+const inputs = document.getElementsByName('radio')
