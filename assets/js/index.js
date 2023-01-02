1dcdf218f4ccc208c815c2ebe60722104495d8ce
Olena Zdanovska <freshjs.ftm@gmail.com>
2023-01-02 13:53:04 +0200

    Start slider

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,14 +1,10 @@
 "use strict";
 
-// v1  a
-const [active1] = document.getElementsByClassName('active');
-// v2  a
-const active2 = document.querySelector('.active');
+const imagesDB = [
+  'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/ocean-quotes-index-1624414741.jpg',
+  'https://media.vogue.fr/photos/62d7c593413ae1c919695f4a/2:3/w_2560%2Cc_limit/155380802',
+  'https://th-thumbnailer.cdn-si-edu.com/HPG2yfboB6qxZb-E3cayS7dYu5s=/1072x720/filters:no_upscale():focal(2301x1599:2302x1600)/https://tf-cmsv2-smithsonianmag-media.s3.amazonaws.com/filer_public/a3/9c/a39cb32f-b19e-4c45-a236-d0cb54c0be8f/5840602412_0611190a2a_o.jpg'
+];
 
-
-//v1 ps
-const ps = document.querySelectorAll('h2+p');
-
-//const ps = document.getElementsByTagName('h2+p');
-
-const inputs = document.getElementsByName('radio')
+const img = document.querySelector(".slider-container>.slide>img");
+const [prevBtn, nextBtn] = document.querySelectorAll(".slider-container button");
