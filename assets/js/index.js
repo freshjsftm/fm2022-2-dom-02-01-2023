1c2e2738626e6d0f533f0b8cf8198980dd605829
Olena Zdanovska <freshjs.ftm@gmail.com>
2023-01-02 11:09:47 +0200

    Prepare task

--- assets/js/index.js
+++ assets/js/index.js
@@ -4,12 +4,3 @@ function showMessage(msg = "1111") {
   console.log(msg);
 }
 
-//tag
-const lisTag = document.getElementsByTagName('li')
-
-//class
-const lisClass = document.getElementsByClassName('item')
-
-//all li
-const lisAll = document.querySelectorAll('.item');
-const ps = document.querySelectorAll('article>p');
