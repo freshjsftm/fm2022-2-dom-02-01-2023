b8808914961facee49c85843db6f36cc7de5ca28
Olena Zdanovska <freshjs.ftm@gmail.com>
2023-01-03 12:10:19 +0200

    Start hw

--- assets/js/index.js
+++ assets/js/index.js
@@ -5,6 +5,7 @@ const imagesDB = [
   "https://media.vogue.fr/photos/62d7c593413ae1c919695f4a/2:3/w_2560%2Cc_limit/155380802",
   "https://th-thumbnailer.cdn-si-edu.com/HPG2yfboB6qxZb-E3cayS7dYu5s=/1072x720/filters:no_upscale():focal(2301x1599:2302x1600)/https://tf-cmsv2-smithsonianmag-media.s3.amazonaws.com/filer_public/a3/9c/a39cb32f-b19e-4c45-a236-d0cb54c0be8f/5840602412_0611190a2a_o.jpg",
 ];
+
 const slider = new Slider(imagesDB);
 
 const img = document.querySelector(".slider-container>.slide>img");
@@ -14,6 +15,7 @@ const [prevBtn, nextBtn] = document.querySelectorAll(
 
 function updateViewSlider() {
   img.setAttribute("src", slider.currentSlide);
+  //дз - додати атрибути alt та title з текстом
 }
 updateViewSlider();
 
