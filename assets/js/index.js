31275a565aa62d7382f11fdbcfd92db9d1f9f66d
Olena Zdanovska <freshjs.ftm@gmail.com>
2023-01-09 12:11:15 +0200

    Add template regexp & hw

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,5 +1,12 @@
 "use strict";
 
+const template1 = /^\w{1,14}$/g;
+const template2 = new RegExp('^\\w{1,14}$', "g");
+
+//дз
+//написати регулярний вираз, який буде валідувати  
+//ім'я файлу картинки з розширенням png або webp
+
 const userMessagesArray = [];
 const messagesList = document.getElementById("messages");
 const rootForm = document.getElementById("root-form");
@@ -12,10 +19,11 @@ function handlerForm(event) {
   } = event;
   event.preventDefault();
   const userInput = textInput.value.trim();
-  if (userInput) {//validation 
+  if (template2.test(userInput)) {
+    //validation
     userMessagesArray.push(userInput);
     // створити новий пункт і додати його до списку
-    const li = createElement('li',{}, userInput);
+    const li = createElement("li", {}, userInput);
     messagesList.append(li);
 
     target.reset();
