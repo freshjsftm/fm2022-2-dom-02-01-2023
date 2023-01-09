f18c521a4b2e62d579aa57bd588d69781fb6cc51
Olena Zdanovska <freshjs.ftm@gmail.com>
2023-01-09 09:40:32 +0200

    Add message to array

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,22 +1,24 @@
 "use strict";
 
+const userMessagesArray = [];
+
 const rootForm = document.getElementById("root-form");
-//const [, btnSubmit] = rootForm.elements;
-//console.log(btnSubmit);
-// console.dir(rootForm)
-// console.dir(rootForm.elements.textInput)
-
-// btnSubmit.addEventListener("click", (event) => {
-//   event.preventDefault();
-//   console.log('event.type:', event.type);
-// });
-
 rootForm.addEventListener("submit", handlerForm);
 
 function handlerForm(event) {
   const {
+    target,
     target: { textInput },
   } = event;
   event.preventDefault();
   console.log("value:", textInput.value);
+  console.log(target);
+  const userInput = textInput.value.trim();
+  //додавати введене значення до масиву userMessagesArray
+  if (userInput) {
+    userMessagesArray.push(userInput);
+    //очищати форму після додавання
+    target.reset();
+  }
+  console.log(userMessagesArray);
 }
