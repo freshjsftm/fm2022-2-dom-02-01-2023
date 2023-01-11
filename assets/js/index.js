8132914a620e39cb82321e80965e56107cedf7f3
Olena Zdanovska <freshjs.ftm@gmail.com>
2023-01-11 12:12:10 +0200

    Add new Promise

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,46 +1,24 @@
 "use strict";
-//const promise = fetch("ht://");
-// const promise = fetch("./assets/js/data.json");
-// console.log(promise);
 
-// const jsonPromise = promise.then((response)=>{
-//   return response.json();
-// })
+const myPromise1 = new Promise(executor);
 
-// jsonPromise.then((data)=>{
-//   console.table(data);
-// })
+function executor(resolve, reject) {
+  Math.random() > 0.5 ? resolve('ok') : reject('error');
+}
 
+myPromise1
+.then((data)=>{console.log('resolve:', data)})
+.catch((err)=>{console.log('reject:', err)})
 
-// promise.then(
-//   //fullfild
-//   (response) => {
-//     // console.log("OK");
-//     // console.log('data here=>',response)
-//     const jsonPromise = response.json();
-//     jsonPromise.then(
-//       (data) => {
-//         console.table(data);
-//       },
-//       () => {
-//         console.log("error data");
-//       }
-//     );
-//   },
-//   //rejected
-//   () => {
-//     console.log("error");
-//   }
-// );
+//setTimeout(callback, 1000);
 
+// function timeout(ms){
+//   return new Promise((resolve, reject)=>{
+//     setTimeout(reject, ms);
+//   })
+// }
 
-fetch("./assets/js/data.json")
-  .then((response) => response.json())
-  .then((data) => {
-    console.table(data);
-    //виведіть імена юзерів
-    data.forEach((user)=>{console.log(user.firstName)})
-  })
-  .catch((error)=>{console.log(error)})
-  .finally(()=>{console.log('finally')})
-
+// timeout(3000)
+//   .then(()=>{console.log('resolve')})
+//   .catch(()=>{console.log('error')})
+//   .finally(()=>{console.log('finally')})
