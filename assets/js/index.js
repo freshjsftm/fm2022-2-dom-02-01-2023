47431a8a7a8f24c5c8d32d91365af5d242d7dfbc
Olena Zdanovska <freshjs.ftm@gmail.com>
2023-01-12 11:08:12 +0200

    Add example delegation event

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,47 +1,11 @@
-'use strict';
-// console.log("start");///s
+"use strict";
 
-// setTimeout(() => {  
-//   console.log("setTimeout");//macro
-// }, 0);
+const list = document.getElementById("list");
+list.addEventListener("click", handlerClick);
 
-// const pr1 = new Promise((resolve, reject) => {
-//   console.log("Promise start");///s
-//   resolve();
-//   //reject();
-//   console.log("Promise end");///s
-// });
-
-// pr1
-//   .then(() => {
-//     console.log("resolve");//micro
-//   })
-//   .catch(() => {
-//     console.log("reject");//micro
-//   })
-//   .finally(() => {
-//     console.log("finally");//micro
-//   });
-
-// console.log("end");///s
-
-
-
-function handlePromise(promise){
-  return promise.then((data)=>{
-    console.log('resolve data:', data);
-  }).catch((err)=>{
-    console.log('reject error:', err);
-  })
+function handlerClick({target}){
+  if(target===list) return;
+  console.log(target.innerText)
+  target.innerText += '!'
+  target.style.color = 'teal';
 }
-
-const value = 45;
-
-// const valuePromise = new Promise((resolve, reject)=>{
-//   resolve(value)
-// })
-
-//const valuePromise = Promise.resolve(value);
-const valuePromise = Promise.reject(value);
-
-handlePromise(valuePromise);
