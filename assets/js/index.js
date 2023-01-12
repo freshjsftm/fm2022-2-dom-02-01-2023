1c9fdf77eb72223b75092d488d748bddc00faeac
Olena Zdanovska <freshjs.ftm@gmail.com>
2023-01-12 13:58:38 +0200

    Start hw

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,11 +1,24 @@
 "use strict";
 
-const list = document.getElementById("list");
-list.addEventListener("click", handlerClick);
+// then((data)=>{
+//   const dataActors = data.filter((d)=>d.firstName!=='' && d.profilePicture!==null);
+//   dataActors.forEach();
+// })
 
-function handlerClick({target}){
-  if(target===list) return;
-  console.log(target.innerText)
-  target.innerText += '!'
-  target.style.color = 'teal';
-}
+const socialMap = new Map();
+socialMap.set("www.facebook.com", "fb-icon");
+socialMap.set("twitter.com", "tw-icon");
+socialMap.set("www.instagram.com", "in-icon");
+socialMap.set("www.tiktok.com", "tt-icon");
+
+const sources = [
+  "https://www.facebook.com/DwayneJohnson",
+  "https://www.instagram.com/therock/?hl=ru"
+]
+
+const links = sources.map((src)=>{
+  const socialIcon = socialMap.get(new URL(src).host);
+  return createElement('a', {classNames:['icon', socialIcon], attrs:{'href':src, 'target':'_blank'}});
+})
+
+document.getElementById('root').append(...links);
